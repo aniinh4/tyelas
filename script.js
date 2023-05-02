a27a278d01efb47ec4930ede3d52bb2606c4985b
Ana Oliveira <125682493+aniinh4@users.noreply.github.com>
2023-05-02 19:01:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <!DOCTYPE html><html><head>
+    <link href="https://fonts.googleapis.com/css?family=Economica&display=swap" rel="stylesheet" />
+  </head>
+    <div class="classe15">
+      <span class="Horário">Hórario</span>
+      <span class="Escolhaseudia">Escolha seu dia></span>
+      <span class="Agendar">Agendar</span><div class="classe14"></div>
+      <div class="classe4">
+       
+        
+      
+      </div>
+      <div class="classe5">
+        
+      </div><div class="classe7">
+      </div>
+      <span class="Agendadas"> Agendadas</span>
+      <span class="Passadas">Passadas</span>
+      <div class="classe26"></div>
+      <span class="menu">menu</span>
+      <div class="name"></div>
+      <div class="name"></div>
+      <div class="name"></div>
+      <div class="classe8"></div>
+      <div class="classe9"></div>
+      <div class="classe10"></div>
+      <div class="classe11"></div>
+      <div class="classe6"></div><span class="Especialistas">especialistas...</span>
+      <div class="classe12"></div>
+      <div class="classe13"></div>
+      <span class="escolhas">escolhas...</span><span class="escolhas1">escolhas...</span><div class="classe3"></div></div></html> <br/><br/>  
+  <img src="menu.png" class="menu2">
+    </form>
+  <form>
+      <p></p><input type="text" name="escolhas..."  placeholder="escolhas..." class="botoes">
+      <p></p><input type="password" name="escolhas..."   placeholder="escolhas...:" class="botoes">
+           
+</form>
+</html>
